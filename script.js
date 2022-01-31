5d768ee31bd6f8d4403c81b3a2c2c45ccf8678ac
kimdonghyeon-97 <78817995+kimdonghyeon-97@users.noreply.github.com>
2022-02-01 03:36:12 +0900

    comments change

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-/*
-[JavaScript]
 const loadText = document.querySelector('.loading-text')
-: loading-text class를 지정하는 함수
-const bg = document.querySelector('.bg')
-: bg class를 지정하는 함수 
+// loading-text class를 지정하는 함수
 
-*/
-
-const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
+// bg class를 지정하는 함수 
 
 let load = 0
 
 let int = setInterval(blurring, 30)
+//  blurring 함수를 30ms 간격으로 실행
 
 function blurring() {
     load++
@@ -24,6 +19,7 @@ function blurring() {
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    // scale의 4번째 인자의 값이 커질수록 더 블러처리됨
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
